f7ad29588bda51831e36deecb8ec5c365e5fe567
mac <mac@macs-MacBook-Pro.local>
2017-03-09 18:24:14 +0700

    before preview

---
+++ public_html/sites/all/modules/custom/bc/templates/add_ticket.tpl.php
@@ -0,0 +1,10 @@
+<div class="add-ticket">
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtDateTime"/></div>
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtStart"/>  </div>
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtEnd"/>  </div>
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtType"/>  </div>
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtNum"/></div>  
+    <div class="col-sm-2 col-xs-6"><input type="text" placeholder="" class="txtNote"/>  </div>
+        
+    <i class="fa fa-check" aria-hidden="true" title="Add this line to list"></i>
+</div>
