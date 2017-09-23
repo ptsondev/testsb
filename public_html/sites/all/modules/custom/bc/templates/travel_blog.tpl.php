a604241b4d36a509c400b9f30aa99a38f08563ca
mac <mac@macs-MacBook-Pro.local>
2017-09-23 09:37:36 +0700

    done blog

--- public_html/sites/all/modules/custom/bc/templates/travel_blog.tpl.php
+++ public_html/sites/all/modules/custom/bc/templates/travel_blog.tpl.php
@@ -1,51 +1,49 @@
 <?php
-$nids = db_query_range('SELECT nid FROM node WHERE type=:type AND status=1', 0, 10, array(':type' => 'post'))->fetchCol();
+
+?>
+
+<?php
+$nids = db_query_range('SELECT nid FROM node WHERE type=:type AND status=1', 0, 30, array(':type' => 'post'))->fetchCol();
 $posts = node_load_multiple($nids);
 
 foreach ($posts as $post) {
+    $account = user_load($post->uid);
     echo '<div class="u-post" id="u-post-'.$post->nid.'">';
         echo '<div class="u-post-info">';
             echo '<div class="row">';
-                $account = user_load($post->uid);
                 echo '<div class="col-sm-1 col-xs-2"><a href="'.url('user/'.$post->uid).'"><img src="'.image_style_url('square', $account->field_avatar[LANGUAGE_NONE][0]['uri']).'" /></a></div>';
-                echo '<div class="col-sm-11 col-xs-10">';                    
+                echo '<div class="col-sm-11 col-xs-10">';
                     echo '<a href="'.url('user/'.$post->uid).'">'.getUserName($account).'</a>';
                     echo '<div class="u-post-date">'. date('d-M-Y h:i:s', $post->created).'</div>';
                 echo '</div>';
             echo '</div>';
         echo '</div>';
         
-        echo '<div class="u-post-main">';        
-            echo '<h3 class="u-post-title"><a href="'.url('node/'.$post->nid).'">' . $post->title . '</a></h3>';
-            display_node_summary($post);
+        echo '<div class="u-post-main">';  
+            echo '<div class="sum">';
+                echo $post->field_summary[LANGUAGE_NONE][0]['value'];
+            echo '</div>';    
+            
+            echo '<div class="post-main-content row">';         
+                echo '<div class="col-sm-4"><img src="'.image_style_url('width_2_height',$post->field_image[LANGUAGE_NONE][0]['uri']).'" /></div>';
+                echo '<div class="col-sm-8">';
+                    echo '<h3 class="u-post-title">' . $post->title . '</h3>';
+                    echo text_summary($post->field_content[LANGUAGE_NONE][0]['value'], $format = NULL, $size = 150).'...';
+                echo '</div>';
+            echo '</div>';
         echo '</div>';
 
         echo '<div class="u-post-tool">';
-            echo '<i class="fa fa-comments" aria-hidden="true" data-nid="'.$post->nid.'"> Comment</i>';
-                        
-            //echo flag_create_link('like', $post->nid);            
-            echo '<div class="fb-like" data-href="'.url('node/'.$post->nid).'" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>';
+            echo '<a class="twitter-share-button"href="https://twitter.com/intent/tweet">Tweet</a>';
+            echo '<div class="ggplus-like"><script src="https://apis.google.com/js/platform.js" async defer></script><g:plusone></g:plusone></div>';
+            echo '<div class="fb-like" data-href="'.url('node/'.$post->nid).'" data-layout="standard" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>';
+            
+            
         echo '</div>';
         
-        $form = drupal_get_form("comment_node_post_form", (object) array('nid' => $post->nid));
-        print drupal_render($form);
+       $cur_url = url('node/'.$post->nid, array('absolute' => TRUE)); 
+                    echo '<div class="fb-comments" data-href="'.$cur_url.'" data-numposts="5"></div> ';             
         
-        $cids = comment_get_thread($post, COMMENT_MODE_THREADED, 15);
-        $comments = comment_load_multiple($cids);
-        foreach($comments as $comment){
-            //var_dump($comment);
-            echo '<div class="u-post-comment">';
-                $account_comment = user_load($comment->uid);
-                echo '<div class="user-info row">';                    
-                    echo '<div class="col-sm-1 col-xs-2"><a href="'.url('user/'.$comment->uid).'"><img src="'.image_style_url('square', $account_comment->field_avatar[LANGUAGE_NONE][0]['uri']).'" /></a></div>';
-                    echo '<div class="col-sm-11 col-xs-10">';
-                        echo '<a href="'.url('user/'.$comment->uid).'">'.getUserName($account_comment).'</a> <span class="comment-body">'.$comment->comment_body[LANGUAGE_NONE][0]['value'].'</span>';
-                        echo '<div class="u-post-date">'. date('d-M-Y h:i:s', $comment->created).'</div>';
-                    echo '</div>';
-                echo '</div>';
-                
-            echo '</div>';
-        }
         
     echo '</div>';
 }
